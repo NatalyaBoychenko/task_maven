68483d4b89661ce88ab11bd51a1a0ba8eaef2eca
Natalya Boichenko <natalya.andriychuk@gmail.com>
2024-03-30 19:44:33 +0200

    Initial commit

---
+++ src/main/java/com/boichenko/App.java
@@ -0,0 +1,24 @@
+package com.boichenko;
+
+import com.google.gson.Gson;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Map<String, String> name = new LinkedHashMap<>();
+
+        name.put("name", "Natalia");
+        name.put("lastName", "Boichenko");
+
+        String json = new Gson().toJson(name);
+        System.out.println(json);
+    }
+}
